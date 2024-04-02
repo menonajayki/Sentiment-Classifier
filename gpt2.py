715804f4817e00637862288e8616600ad85a60c3
Ajay <menonajayki@gmail.com>
2024-04-02 02:16:58 +0200

    testing 2 and 2-xl

--- gpt2.py
+++ gpt2.py
@@ -5,10 +5,12 @@ model = GPT2LMHeadModel.from_pretrained('gpt2')
 
 def text_generator(text):
     encode_text = tokenizer.encode(text, return_tensors='pt')
-    decode_text = tokenizer.decode(encode_text[0], skip_special_tokens=True)
     print("Encoded Text:")
     print(encode_text)
-    print("Decoded Text:")
+    next_sentences = model.generate(encode_text)
+    decode_text = tokenizer.decode(next_sentences[0], skip_special_tokens=True)
+    print("Generated Text:")
     print(decode_text)
 
-text_generated = text_generator("Hello my world")
+
+text_generated = text_generator("best friends")
